16a1de874285e5b4ee6c7664759053ab8323afd0
Zakhar Veremchuk <zaharverem@gmail.com>
2024-07-13 11:44:27 +0300

    Save commit

--- task4.py
+++ task4.py
@@ -1,4 +1,5 @@
-
+from datetime import datetime
+import re 
 
 # У межах вашої організації, ви відповідаєте за організацію привітань колег з днем народження.
 # Щоб оптимізувати цей процес, вам потрібно створити функцію get_upcoming_birthdays,
@@ -8,4 +9,23 @@
 # Оскільки дні народження колег можуть припадати на вихідні, ваша функція також повинна враховувати це та переносити дату привітання на наступний робочий день, якщо необхідно.
 
 def get_upcoming_birthdays(users: dict[str, str]):
-    pass 
+    today = datetime.today().date()
+    next_week_birthday = dict()
+    for user in users:
+        name = user["name"]
+        birthday = re.sub(r'\b\d{4}\b', str(today.year), user["birthday"])
+        birthday = datetime.strptime(birthday, "%Y.%m.%d").date()
+        if (birthday - today).days <= 7:
+            next_week_birthday['name'] = name
+            next_week_birthday['birthday'] = datetime.strftime(birthday, '%Y.%m.%d')
+    
+    return next_week_birthday
+
+        
+
+users = [
+    {"name": "John Doe", "birthday": "1985.07.09"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"}
+]
+
+print(get_upcoming_birthdays(users))
